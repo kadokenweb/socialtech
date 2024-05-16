a937c7f8f0b2e2dc6bb2e4b15f712da8839acf12
kadokenweb <165865865+kadokenweb@users.noreply.github.com>
2024-05-16 09:07:03 +0900

    テスト　PHP

---
+++ ajax.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <button id="refresh-btn">更新</button>
+    <div id="outputDiv">SAMURAI</div>
+
+    <script src="ajax_js.js"></script>
+
+</body>
+
+</html>
